f731113f0dabecfe36bfb638c0e8a62fd820b703
Mike Taylor <mike@indexdata.com>
2023-10-23 15:32:08 +0100

    Support variable sort order

--- queries/courses_by_title.sql
+++ queries/courses_by_title.sql
@@ -1,21 +1,23 @@
 --ldp:function get_courses
 
 CREATE FUNCTION get_courses(
-    minimum_number_of_students integer DEFAULT '0',
-    maximum_number_of_students integer DEFAULT '2147483647'
+    minimum_number_of_students bigint DEFAULT '0',
+    maximum_number_of_students bigint DEFAULT '2147483647',
+    sort_order text DEFAULT 'name'
 )
 RETURNS TABLE(
     id text,
     course_number text,
     name text,
     description text,
-    number_of_students integer,
+    number_of_students bigint,
     section_name text)
 AS $$
 SELECT id::text, course_number, name, description, number_of_students, section_name
     FROM course_courses
     WHERE course_courses.number_of_students >= minimum_number_of_students
       AND course_courses.number_of_students <= maximum_number_of_students
+    ORDER BY sort_order
 $$
 LANGUAGE SQL
 STABLE
